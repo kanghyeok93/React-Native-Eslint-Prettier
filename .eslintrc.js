a17d4b1b24a369d71dbd496c05028f11c8086298
서강혁 <rkdgur7007@gmail.com>
2021-06-29 10:49:15 +0900

    feat(eslint.js): eslint 파일 초기 설정

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,13 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  extends: [
+    'eslint:recommended',
+    '@react-native-community',
+    'airbnb',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    // example) 'react/prop-types': 'warn',
+  },
 };
